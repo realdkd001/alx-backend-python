16e0239e687e2a9b599dae74d14ed4174f2d1c49
Dzrekey001 <dkdzrekey001@gmail.com>
2025-07-05 16:09:59 +0000

    feat: add memory-efficient average age calculation using generator

--- python-generators-0x00/4-stream_ages.py
+++ python-generators-0x00/4-stream_ages.py
@@ -1,8 +1,7 @@
 from contextlib import closing
 seed = __import__('seed')
 
-
-def stream_user_age():
+def stream_user_ages():
     with closing(seed.connect_to_prodev()) as cnx:
         with closing(cnx.cursor(dictionary=True)) as cursor:
             cursor.execute("SELECT age FROM user_data;")
@@ -12,7 +11,7 @@ def stream_user_age():
 def calculate_average_age():
     total_age = 0
     total_user = 0
-    for user in stream_user_age():
+    for user in stream_user_ages():
         total_age += user["age"]
         total_user += 1
         
