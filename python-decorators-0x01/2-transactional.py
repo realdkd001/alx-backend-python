8c45c36c4033e9ce347a283de725efc7fad2cfe9
Dzrekey001 <dkdzrekey001@gmail.com>
2025-07-10 10:27:11 +0000

    feat: implement @transactional decorator for safe DB updates with rollback on failure

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,35 @@
+import sqlite3 
+import functools
+
+with_db_connection = __import__("1-with_db_connection").with_db_connection
+
+"""your code goes here"""
+def transactional(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        
+        cnx = args[0] if args else kwargs.get('conn')
+        
+        if cnx is None:
+            raise ValueError("No database connection provided to the function.")
+
+        try:
+            result = func(*args, **kwargs)
+            cnx.commit()
+            print(f"Transaction committed: {func.__name__}()")
+            return result
+        except Exception as e:
+            cnx.rollback()
+            print(f"Transaction failed in {func.__name__}(): {e}. Rolled back.")
+         
+    return wrapper
+
+
+@with_db_connection 
+@transactional 
+def update_user_email(conn, user_id, new_email): 
+    cursor = conn.cursor() 
+    cursor.execute("UPDATE users SET new_email = ? WHERE id = ?", (new_email, user_id)) 
+    #### Update user's email with automatic transaction handling 
+
+update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
