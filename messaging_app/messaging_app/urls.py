a2106d5d16ee47e46a192158fa295301e9aeb882
Dzrekey001 <dkdzrekey001@gmail.com>
2025-07-17 01:06:55 +0000

    Add filters and status imports to support search and custom responses in viewsets

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import  DefaultRouter
+from chats.views import ConversationViewSet, MessageViewSet
+
+router = DefaultRouter()
+router.register(r'conversation', ConversationViewSet, basename="conversation")
+router.register(r'message', MessageViewSet, basename='message')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("chats.urls"))
+    path('api/', include(router.urls))
 ]
