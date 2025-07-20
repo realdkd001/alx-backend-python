3af78c836284c88cbe73859cf21ecf48d591453d
Dzrekey001 <dkdzrekey001@gmail.com>
2025-07-20 17:25:12 +0000

    Add unit test for KeyError exceptions in access_nested_map using parameterized inputs

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -12,3 +12,11 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected ):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+        
+    @parameterized.expand([
+        ({}, ['a']),
+        ({'a': 1}, ["a", "b"])
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError):
+            self.assertEqual(access_nested_map(nested_map, path))
